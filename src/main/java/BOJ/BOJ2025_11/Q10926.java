ec6660f9d94f63d471f2e9afbff3fcad5d711b20
suminiee <chansu0114@naver.com>
2025-11-12 19:37:47 +0900

    2025-11-12 / Q10926

---
+++ src/main/java/BOJ/BOJ2025_11/Q10926.java
@@ -0,0 +1,12 @@
+package BOJ.BOJ2025_11;
+import java.util.*;
+import java.io.*;
+public class Q10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        sb.append(name).append("??!");
+        System.out.println(sb);
+    }
+}
